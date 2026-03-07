205a8b0422a1fd82b4c302a4d147d6fdc62c0a16
talaverano1 <nicolasvegamalve@gmail.com>
2026-03-06 22:06:51 -0300

    Initial commit

---
+++ src/main/java/com/equifitapp/EquifitappApplication.java
@@ -0,0 +1,13 @@
+package com.equifitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquifitappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquifitappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equifitapp/EquifitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equifitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquifitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
